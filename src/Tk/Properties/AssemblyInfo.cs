fcd355855d2e51a2dffdb8567563ddcf4630c6a2
Jeff Wilcox <jwilcox@microsoft.com>
2012-06-14 15:14:22 -0700

    Adding my fork of the Silverlight for Windows Phone Toolkit. I have a subset compiled in and a custom version of transitions.

---
+++ src/Tk/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tk")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Silverlight for Windows Phone Toolkit")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("71342b9a-5fca-4864-8c0d-c4daa49713bd")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/Tk/Transitions/ITransitionCompleted.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// Jeff Wilcox custom code.
+
+namespace Microsoft.Phone.Controls
+{
+    public interface ITransitionCompleted
+    {
+        void OnTransitionCompleted();
+        void OnTransitionGoodbyeTemporary();
+    }
+}
