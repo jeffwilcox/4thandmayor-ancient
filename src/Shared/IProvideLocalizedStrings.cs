c1fb46092b0e04fbdcdedf53e7ccc2e47fc8dad3
Jeff Wilcox <jeffwilcox@gmail.com>
2012-08-02 13:36:13 -0700

    updating with app helpers for localization

--- src/Shared/IProvideLocalizedStrings.cs
+++ src/Shared/IProvideLocalizedStrings.cs
@@ -1,8 +1,38 @@
 ﻿
+using System.Windows;
+using System;
+
 namespace JeffWilcox.Controls
 {
+    // refactor out sometime
+
+    public static class ApplicationLocalizationExtensions
+    {
+        public static bool TryGetLocalizedString(this Application app, string key, out string @string)
+        {
+            return RequireAppLocalizationInterface(app).TryGetLocalizedString(key, out @string);
+        }
+
+        private static IProvideLocalizedStrings RequireAppLocalizationInterface(Application app)
+        {
+            var appStrings = app as IProvideLocalizedStrings;
+            if (appStrings != null)
+            {
+                return appStrings;
+            }
+
+            throw new InvalidOperationException("The Application does not support IProvideLocalizedStrings.");
+        }
+
+        public static string GetLocalizedString(this Application app, string key)
+        {
+            return RequireAppLocalizationInterface(app).GetLocalizedString(key);
+        }
+    }
+
     public interface IProvideLocalizedStrings
     {
         string GetLocalizedString(string identifier);
+        bool TryGetLocalizedString(string key, out string @string);
     }
 }
