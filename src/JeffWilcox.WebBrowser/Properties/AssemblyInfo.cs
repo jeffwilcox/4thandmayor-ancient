9fcf3f261a9e55c3a4d2dfce7fc90a80b0df7566
Jeff Wilcox <jeffwilcox@gmail.com>
2012-08-02 13:45:44 -0700

    updating copyright headers

--- src/JeffWilcox.WebBrowser/Properties/AssemblyInfo.cs
+++ src/JeffWilcox.WebBrowser/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿//
-// Copyright (c) 2010-2011 Jeff Wilcox
+// Copyright (c) Jeff Wilcox
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
